cd2decffb233a24932413790a27afde93d65ad84
Sandro Alvarez and Satchel Grant <sandro@>
2016-08-29 11:39:27 -0700

    add snake-like movement

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,31 @@
-function SnakeBit(x,y, head, context){
+function SnakeBit(x,y, context){
   this.xc = x;
   this.yc = y;
   this.squareSize = 9;
   this.context = context;
-  this.head = head;
 }
-SnakeBit.prototype.slither = function() {
+SnakeBit.prototype.updateBit = function() {
   this.context.fillRect(this.xc, this.yc, this.squareSize, this.squareSize);
 }
 function Snake(context) {
-  var headBit = new SnakeBit(100,300, true, context);
+  var initialX = 100;
+  var initialY = 300;
+  var headBit = new SnakeBit(initialX,initialY, context);
   this.bits = [headBit];
+  for (var i = 1; i < 4; i++) {
+    var tailBit = new SnakeBit(initialX-(10*i),initialY, context);
+    this.bits.push(tailBit);
+  }
   this.gameOver = false;
 }
-
+Snake.prototype.updateBits = function() {
+  this.bits[0].updateBit();
+  for (var count = this.bits.length-1; count > 0; count--) {
+    this.bits[count].xc = this.bits[count-1].xc;
+    this.bits[count].yc = this.bits[count-1].yc;
+    this.bits[count].updateBit();
+  }
+}
 $(document).ready(function() {
   $(document.body).on('keydown', onkeypress);
 
@@ -55,35 +67,38 @@ $(document).ready(function() {
 
   var xCoord = null;
   var yCoord = null;
-  var gameSpeed = 1;
+  var gameSpeed = 100;
+  var prevTimestamp = null;
   var snakeGuy = new Snake(ctx);
 
   var startTime = null;
-  window.requestAnimationFrame(function step() {
+  window.requestAnimationFrame(function step(timestamp) {
+    if(timestamp > prevTimestamp + gameSpeed) {
+      prevTimestamp = timestamp;
+      ctx.fillStyle = 'white';
+      ctx.fillRect(0,0,c.clientWidth,c.clientHeight);
 
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0,0,c.clientWidth,c.clientHeight);
-
-    ctx.beginPath();
-    ctx.fillStyle = "black";
-    if (left == true) {
-      snakeGuy.bits[0].xc -= gameSpeed;
-      snakeGuy.bits[0].slither();
-      ctx.stroke();
-    } else if (right == true) {
-      snakeGuy.bits[0].xc += gameSpeed;
-      snakeGuy.bits[0].slither();
-      ctx.stroke();
-    }  else if (down == true) {
-      snakeGuy.bits[0].yc += gameSpeed;
-      snakeGuy.bits[0].slither();
-      ctx.stroke();
-    }  else if (up == true) {
-      snakeGuy.bits[0].yc -= gameSpeed;
-      snakeGuy.bits[0].slither();
-      ctx.stroke();
+      ctx.beginPath();
+      ctx.fillStyle = "black";
+      if (left == true) {
+        snakeGuy.bits[0].xc -= 10;
+        snakeGuy.updateBits();
+        ctx.stroke();
+      } else if (right == true) {
+        snakeGuy.bits[0].xc += 10;
+        snakeGuy.updateBits();
+        ctx.stroke();
+      }  else if (down == true) {
+        snakeGuy.bits[0].yc += 10;
+        snakeGuy.updateBits();
+        ctx.stroke();
+      }  else if (up == true) {
+        snakeGuy.bits[0].yc -= 10;
+        snakeGuy.updateBits();
+        ctx.stroke();
+      }
     }
-
+    console.log(timestamp);
     window.requestAnimationFrame(step);
   });
 });
