c14daeb99ec965083f1c4d9dfbcec192bf9e3746
Sandro Alvarez and Satchel Grant <satchel@>
2016-08-29 14:38:12 -0700

    add item that regenerate once consumed and comments for clarification

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
+//game class definitions
+//create each snake piece
 function SnakeBit(x,y, context){
   this.xc = x;
   this.yc = y;
   this.squareSize = 9;
   this.context = context;
 }
+//draws each snake piece
 SnakeBit.prototype.updateBit = function() {
   this.context.fillRect(this.xc, this.yc, this.squareSize, this.squareSize);
 }
+//holds snake body information
 function Snake(context) {
   this.initialX = 100;
   this.initialY = 300;
@@ -21,6 +25,7 @@ function Snake(context) {
   this.gameOver = false;
   context.stroke();
 }
+//Synchronizes snake movement
 Snake.prototype.updateBits = function() {
   this.bits[0].updateBit();
   for (var count = this.bits.length-1; count > 0; count--) {
@@ -30,16 +35,8 @@ Snake.prototype.updateBits = function() {
   }
 }
 $(document).ready(function() {
-  $(document.body).on('keydown', onkeypress);
-
-  var c = document.getElementById('canvas');
-  var ctx = c.getContext('2d');
-  var up = false;
-  var down = false;
-  var left = false;
-  var right = true;
-
-  function onkeypress(key) {
+//receives user input
+  $(document.body).on('keydown', function onkeypress(key) {
     switch(key.keyCode) {
       case 40:
         down = true;
@@ -66,20 +63,29 @@ $(document).ready(function() {
         down = false;
         break;
     }
-  }
+  });
 
-  var xCoord = null;
-  var yCoord = null;
-  var gameSpeed = 200;
+//declares initial game variables
+  var c = document.getElementById('canvas');
+  var ctx = c.getContext('2d');
+  var up = false;
+  var down = false;
+  var left = false;
+  var right = true;
+  var gameSpeed = 100;
   var prevTimestamp = null;
 
+//creates snake object and initial images
   var snakeGuy = new Snake(ctx);
   snakeGuy.bits[0].xc += 10;
   snakeGuy.updateBits();
   ctx.fillRect(100, 300, 9, 9);
   console.log(snakeGuy.bits);
+  //creates initial coordinates for item
+  var itemX = Math.floor((Math.random()*800)/10)*10;
+  var itemY = Math.floor((Math.random()*600)/10)*10;
 
-
+//initiates and maintains frame updates
   $("#canvas").click(function() {
     var initialCounter = 0;
     window.requestAnimationFrame(function step(timestamp) {
@@ -92,6 +98,8 @@ $(document).ready(function() {
         ctx.fillStyle = "black";
         ctx.beginPath();
 
+        ctx.fillRect(itemX, itemY, snakeGuy.bits[0].squareSize, snakeGuy.bits[0].squareSize);
+        //tranforms user input into snake movement direction
         if (left == true) {
           snakeGuy.bits[0].xc -= 10;
           snakeGuy.updateBits();
@@ -110,8 +118,15 @@ $(document).ready(function() {
           ctx.stroke();
         }
       }
+      //generates new item placement when snake consumes item
+      if(snakeGuy.bits[0].xc === itemX  && snakeGuy.bits[0].yc === itemY) {
+        itemX = Math.floor((Math.random()*800)/10)*10;
+        itemY = Math.floor((Math.random()*600)/10)*10;
+      }
+      //takes care of white space in snake body during the beginning of the game
       if (initialCounter < snakeGuy.bits.length-2)
         ctx.fillRect(snakeGuy.initialX, snakeGuy.initialY, snakeGuy.bits[0].squareSize, snakeGuy.bits[0].squareSize);
+
       window.requestAnimationFrame(step);
     });
   })
