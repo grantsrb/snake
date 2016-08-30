f3b035245c307e8300785f84fe51815589b5cfab
Satchel Grant <grantsrb@gmail.com>
2016-08-29 18:45:46 -0700

    change most constants to easily manipulated variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 //game class definitions
 //create each snake piece
-function SnakeBit(x,y, context){
+function SnakeBit(x,y, context, squareSizeIn){
   this.xc = x;
   this.yc = y;
-  this.squareSize = 9;
+  this.squareSize = squareSizeIn;
   this.context = context;
 }
 //draws each snake piece
@@ -11,15 +11,15 @@ SnakeBit.prototype.updateBit = function() {
   this.context.fillRect(this.xc, this.yc, this.squareSize, this.squareSize);
 }
 //holds snake body information
-function Snake(context) {
-  this.initialX = 100;
-  this.initialY = 300;
+function Snake(context, xStart, yStart, squareSizeIn) {
+  this.initialX = xStart;
+  this.initialY = yStart;
   context.fillStyle = 'black';
   context.beginPath();
-  var headBit = new SnakeBit(this.initialX,this.initialY, context);
+  var headBit = new SnakeBit(this.initialX,this.initialY, context, squareSizeIn);
   this.bits = [headBit];
   for (var i = 1; i < 6; i++) {
-    var tailBit = new SnakeBit(this.initialX-(10*i),this.initialY, context);
+    var tailBit = new SnakeBit(this.initialX-(10*i),this.initialY, context, squareSizeIn);
     this.bits.push(tailBit);
   }
   context.stroke();
@@ -36,18 +36,18 @@ Snake.prototype.updateBits = function() {
   }
 }
 //adds additional snake body to snake object
-Snake.prototype.snakeBite = function(context) {
-  var tailBit = new SnakeBit(this.bits[2].xc,this.bits[2].yc, context);
+Snake.prototype.snakeBite = function(context, squareSizeIn) {
+  var tailBit = new SnakeBit(this.bits[2].xc,this.bits[2].yc, context, squareSizeIn);
   this.bits.push(tailBit);
 }
 //check for game ending conditions
-Snake.prototype.gameOver = function() {
+Snake.prototype.gameOver = function(canvasWidth, canvasHeight) {
   for (var i = 2; i < this.bits.length; i++) {
     if(this.bits[0].xc === this.bits[i].xc && this.bits[0].yc === this.bits[i].yc) {
       this.gameEnd = true;
     }
   }
-  if(this.bits[0].xc >= 800 || this.bits[0].xc < 0 || this.bits[0].yc >= 600 || this.bits[0].yc < 0) {
+  if(this.bits[0].xc >= canvasWidth || this.bits[0].xc < 0 || this.bits[0].yc >= canvasHeight || this.bits[0].yc < 0) {
     this.gameEnd = true;
   }
 }
@@ -81,20 +81,26 @@ $(document).ready(function() {
 //declares initial game variables
   var c = document.getElementById('canvas');
   var ctx = c.getContext('2d');
+  var canvasWidth = 800;
+  var canvasHeight = 600;
+  console.log(canvasWidth);
   var lastKey = 'right';
   var gameSpeed = 50;
   var prevTimestamp = null;
   var preventKeyChange = 'left';
+  var bitSquareSize = 9;
+  var xStart = 100;
+  var yStart = 300;
 
 //creates snake object and initial images
-  var snakeGuy = new Snake(ctx);
+  var snakeGuy = new Snake(ctx, xStart, yStart, bitSquareSize);
   snakeGuy.bits[0].xc += 10;
   snakeGuy.updateBits();
-  ctx.fillRect(100, 300, 9, 9);
-  console.log(snakeGuy.bits);
+  ctx.fillRect(xStart, yStart, bitSquareSize, bitSquareSize);
+
   //creates initial coordinates for item
-  var itemX = Math.floor((Math.random()*800)/10)*10;
-  var itemY = Math.floor((Math.random()*600)/10)*10;
+  var itemX = Math.floor((Math.random()*canvasWidth)/10)*10;
+  var itemY = Math.floor((Math.random()*canvasHeight)/10)*10;
   var itemColor = 'black';
   ctx.fillRect(itemX, itemY, snakeGuy.bits[0].squareSize, snakeGuy.bits[0].squareSize);
 
@@ -153,10 +159,10 @@ $(document).ready(function() {
         } else {
           itemColor = 'black';
         }
-        itemX = Math.floor((Math.random()*800)/10)*10;
-        itemY = Math.floor((Math.random()*600)/10)*10;
+        itemX = Math.floor((Math.random()*canvasWidth)/10)*10;
+        itemY = Math.floor((Math.random()*canvasHeight)/10)*10;
 
-        snakeGuy.snakeBite(ctx);
+        snakeGuy.snakeBite(ctx, bitSquareSize);
         snakeGuy.score += 10;
       }
       //fixes white space in snake body during the beginning of the game
@@ -164,9 +170,9 @@ $(document).ready(function() {
         ctx.fillRect(snakeGuy.initialX, snakeGuy.initialY, snakeGuy.bits[0].squareSize, snakeGuy.bits[0].squareSize);
       }
       //ends game if game ending conditions are true
-      snakeGuy.gameOver();
+      snakeGuy.gameOver(canvasWidth, canvasHeight);
       if(snakeGuy.gameEnd) {
-        ctx.fillStyle = 'white';
+        ctx.fillStyle = 'red';
         ctx.fillRect(0,0,c.clientWidth,c.clientHeight);
       } else {
         window.requestAnimationFrame(step);
